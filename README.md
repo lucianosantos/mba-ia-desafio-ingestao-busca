0fe6bfa0bb598b52a45b37be46b46b71debc5ea3
Luciano Santos <prolps@gmail.com>
2026-03-18 22:37:47 -0300

    feat: simple readme and ingestion feature with throttled models usage

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Desafio MBA Engenharia de Software com IA - Full Cycle
 
-Descreva abaixo como executar a sua solução.
+python -m venv venv
+
+To activate the venv:
+
+In Git Bash, execute source venv/Scripts/activate
+In power Shell, execute venv/Scripts/activate.ps1
+
+Install dependencies
+
+pip install -r requirements.txt
+
+Init DB in Docker
+
+docker compose up -d
--- src/ingest.py
+++ src/ingest.py
@@ -1,13 +1,62 @@
 import os
+import time
+from pathlib import Path
 from dotenv import load_dotenv
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
+from langchain_postgres import PGVector
 
 load_dotenv()
 
+for k in ("GOOGLE_API_KEY", "GOOGLE_EMBEDDING_MODEL", "PG_VECTOR_URL","PG_VECTOR_COLLECTION_NAME"):
+    if not os.getenv(k):
+        raise RuntimeError(f"Environment variable {k} is not set")
+
+current_dir = Path(__file__).parent
 PDF_PATH = os.getenv("PDF_PATH")
 
 def ingest_pdf():
-    pass
+    docs = PyPDFLoader(str(PDF_PATH)).load()
 
+    splits = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150, add_start_index=False).split_documents(docs)
+
+    if not splits:
+        raise RuntimeError("No document splits were created.")
+
+    enriched = [
+        Document(
+            page_content=doc.page_content,
+            metadata={k: v for k, v in doc.metadata.items() if v not in ("", None)}
+        )
+        for doc in splits
+    ]
+
+    ids =[f"doc-{i}" for i in range(len(enriched))]
+
+    embeddings = GoogleGenerativeAIEmbeddings(model=os.getenv("GOOGLE_EMBEDDING_MODEL"))
+    store = PGVector(
+        embeddings=embeddings,
+        collection_name=os.getenv("PG_VECTOR_COLLECTION_NAME"),
+        connection=os.getenv("PG_VECTOR_URL"),
+        use_jsonb=True
+    )
+
+    # This will consume lots of tokens in no time and may hit rate limits
+    store.add_documents(documents=enriched, ids=ids)
+
+    # If your api key has low rate limits, use the following code to add documents with a delay between each request
+    #print(f"Starting ingestion of {len(enriched)} documents with 2s delay between each...")
+    #for i, (doc, doc_id) in enumerate(zip(enriched, ids), start=1):
+    #    print(f"Ingesting document {i}/{len(enriched)} (ID: {doc_id})...")
+    #    store.add_documents(documents=[doc], ids=[doc_id])
+    #    if i < len(enriched):  # Don't delay after the last document
+    #        print(f"Waiting 2 seconds before next document...")
+    #        time.sleep(2)
+
+    print(f"\nIngestion complete!")
 
 if __name__ == "__main__":
     ingest_pdf()
