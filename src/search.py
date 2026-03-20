ccc3b127f3d19224c1efb1c4b786f2b114da2620
Luciano Santos <prolps@gmail.com>
2026-03-19 23:18:52 -0300

    feat: data search feature

--- src/search.py
+++ src/search.py
@@ -1,3 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+from langchain.chat_models import init_chat_model
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_postgres import PGVector
+
+load_dotenv()
+
 PROMPT_TEMPLATE = """
 CONTEXTO:
 {contexto}
@@ -24,6 +35,58 @@ PERGUNTA DO USUÁRIO:
 
 RESPONDA A "PERGUNTA DO USUÁRIO"
 """
+
+for k in ("GOOGLE_API_KEY", "GOOGLE_EMBEDDING_MODEL", "PG_VECTOR_URL","PG_VECTOR_COLLECTION_NAME"):
+    if not os.getenv(k):
+        raise RuntimeError(f"Environment variable {k} is not set")
+    
+# query = "Que horas são?"
+# query = "Quais empresas de turismo vc conhece?"
+# query = "Quais são os maiores faturamentos e suas empresas?"
+query = "Quais são os menores faturamentos e suas empresas?"
+
+def get_related_data(query):
+  embeddings = GoogleGenerativeAIEmbeddings(model=os.getenv("GOOGLE_EMBEDDING_MODEL"))
+
+  store = PGVector(
+      embeddings=embeddings,
+      collection_name=os.getenv("PG_VECTOR_COLLECTION_NAME"),
+      connection=os.getenv("PG_VECTOR_URL"),
+      use_jsonb=True
+  )
+
+  results = store.similarity_search_with_score(query=query, k=10)
+  concatenated_results = ""
+
+  for i, (doc, score) in enumerate(results, start=1):
+      concatenated_results += f"-"*50 + "\n"
+      concatenated_results += f"Resultado {i} ; Score: {score}\n"
+      concatenated_results += f"-"*50 + "\n"
+      concatenated_results += "Texto:\n"
+      concatenated_results += f"Content: {doc.page_content.strip()}\n"
+  
+  return concatenated_results
+
+def invoke_model_with_data(query, data):
+  template_search = PromptTemplate(
+      input_variables=["contexto", "pergunta"],
+      template=PROMPT_TEMPLATE,
+  )
+
+  gemini = init_chat_model("gemini-2.5-flash", model_provider="google_genai", temperature=0)
+
+  pipeline = template_search | gemini | StrOutputParser()
+  result = pipeline.invoke({
+    "contexto": data,
+    "pergunta": query
+  })
+
+  return result
+
+data = get_related_data(query)
+result = invoke_model_with_data(query, data)
+
+print(result)
 
 def search_prompt(question=None):
     pass
